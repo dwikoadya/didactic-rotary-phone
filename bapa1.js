b866e12668b2990ebc00459c259df000e7bf9b56
Dwiko Adya <dwikoadya@gmail.com>
2020-06-13 19:20:43 +0700

    single file

---
+++ bapa1.js
@@ -0,0 +1,13 @@
+// Variable  type Number & String
+
+var a = 93; //number
+var b = 10;
+var c = 6;
+
+var d = 'marc marquez'; //string
+var e = 'lionel messi';
+var f = 'abc';
+
+console.log(a + d);
+console.log(b + e);
+console.log(b + e);
